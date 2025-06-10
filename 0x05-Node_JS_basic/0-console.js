d03c3ca361d9a44b55e0c6c9f752642117f8e28d
hawi-claire <7hawi7@gmail.com>
2025-06-10 22:45:09 +0000

    Add: Everything

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,14 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const di:splays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
